22025fc9beda692f761058905ea8021ba60b6d11
Jose <jvrs.90@gmail.com>
2024-04-11 06:29:43 +0200

    redesign login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <section className="min-h-[calc(100vh-72px)] flex items-center justify-center">
-      <div className="">
+    <section className="min-h-[calc(100vh-72px)] container mx-auto ">
+      <div className="min-h-[calc(100vh-72px)] flex justify-center items-center">
         <LoginForm />
       </div>
     </section>
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-import { FormEvent, useState } from "react"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
+import { Label } from "@radix-ui/react-label"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "./ui/card"
 
 const setCookiesSession = (user: any) => {
     const date = new Date()
@@ -37,13 +39,56 @@ const LoginForm = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(handleOnSubmit)} className="flex flex-col gap-2 justify-between w-full">
-            <Input
-                type="text" id="username" placeholder="Introduce tu nombre de usuario"
-                {...register("username")}
-            />
-            <Button type="submit">Iniciar sesión</Button>
-        </form>
+        <div className="w-full">
+            {/* <form onSubmit={handleSubmit(handleOnSubmit)} className="flex flex-col gap-2 justify-between w-full">
+                <Input
+                
+                    type="text" id="username" placeholder="Introduce tu nombre de usuario"
+                    {...register("username")}
+                />
+                <Button type="submit">Iniciar sesión</Button>
+            </form> */}
+
+
+            <Card className="mx-auto max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-xl">Iniciar sesión</CardTitle>
+                    {/* <CardDescription>
+                        Introduce tu email y contraseña para continuar creando listas de regalo
+                    </CardDescription> */}
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Correo electrónico</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="name@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="password">Contraseña</Label>
+                            <Input id="password" type="password" />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Iniciar sesión
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Inicia sesión con Google
+                        </Button>
+                    </div>
+                    <div className="mt-4 text-center text-sm">
+                        ¿No tienes una cuenca?{" "}
+                        <Link href="#" className="underline">
+                            Regístrate
+                        </Link>
+                    </div>
+                </CardContent>
+            </Card>
+
+        </div>
     )
 }
 
